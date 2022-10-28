d763fa24c917ba716cb0565097fe9554107ee40f
Felix <felix.wielander@gmail.com>
2022-10-28 20:04:00 +0200

    Fix icon usage

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import {Icon} from '../Icon/Icon';
 import Logo from '/assets/logo.svg'
 import Image from 'next/image';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 
 interface NavbarItem {
@@ -27,7 +28,7 @@ export const Header = ({headerData}: Data) => {
                     {headerData.navbarItems.map(item => {
                         return (
                             <li className="group flex items-center gap-2 text-xl hover:text-secondary cursor-pointer duration-100" key={item.text}>
-                                <Icon className="w-6 h-6 group-hover:rotate-[360deg] duration-700" iconName={item.icon}/>
+                                <Icon className="w-6 h-6 group-hover:rotate-[360deg] duration-700" iconName={item.icon as IconProp}/>
                                 {item.text}
                             </li>
                         )
--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import {IconProp, library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {fab} from '@fortawesome/free-brands-svg-icons';
-import {faCheckSquare, faCoffee, faCheck, faSpinner, faBars, faFilter, faBarcode,faPerson,faCode} from '@fortawesome/free-solid-svg-icons'
-library.add(faCheckSquare, faCoffee, faCheck, fab, faSpinner, faBars, faFilter, faBarcode, faPerson, faCode)
-
+import {fas} from '@fortawesome/free-solid-svg-icons'
+library.add(fas, fab);
 
 interface Props {
-    iconName: IconProp | string;
+    iconName: IconProp;
     className?: string;
 }
 
-export const Icon = ({iconName, className}: Props) => {
-    return (
-        <div>
-            {/*// @ts-ignore*/}
-            <FontAwesomeIcon className={className} icon={iconName}/>
-            {/*// @ts-ignore*/}
-            <FontAwesomeIcon className={className} icon={['fab', `${iconName}`]}/>
-        </div>
-
-    )
-
-}
+export const Icon = ({iconName, className}: Props) => <FontAwesomeIcon className={className} icon={iconName} />;
