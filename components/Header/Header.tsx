a7efd8a586d0dc373079d169aa2c61e6f6a326a5
Felix <felix.wielander@gmail.com>
2022-10-29 20:39:01 +0200

    Improve header content + typings

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,20 @@ import Image from 'next/image';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import content from '../../public/content/header.json';
 
+interface NavbarItem {
+    text?: string;
+    icon?: string;
+    url?: string;
+}
+
+interface HeaderContent {
+    logo: string;
+    items: NavbarItem[];
+}
+
 export const Header = () => {
+    const headerContent = content as HeaderContent;
+
     return (
         <header className="fixed w-full h-fit px-2 py-4 bg-white shadow-md z-20">
             <nav className=" container flex justify-between items-center gap-12 text-gray-500">
@@ -12,7 +25,7 @@ export const Header = () => {
                     <Image src={Logo} width={100} height={50} alt="logo" />
                 </div>
                 <ul className="flex items-center gap-12 font-bold">
-                    {content.navbarItems.map(item => {
+                    {headerContent.items.map(item => {
                         return (
                             <li className="group flex items-center gap-2 text-xl hover:text-secondary cursor-pointer duration-100" key={item.text}>
                                 <Icon className="w-6 h-6 group-hover:rotate-[360deg] duration-700" iconName={item.icon as IconProp}/>
