e177e90a55caa2fa32210d769a1b057c01fd1a34
Felix <felix.wielander@gmail.com>
2022-10-29 20:26:07 +0200

    Remove header data types and cast

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,18 +2,7 @@ import {Icon} from '../Icon/Icon';
 import Logo from '/assets/logo.svg'
 import Image from 'next/image';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import data from '../../public/content/header.json';
-
-interface NavbarItem {
-    text: string;
-    icon: string;
-    url: string;
-}
-
-export interface HeaderData {
-    logo: string;
-    navbarItems: NavbarItem[];
-}
+import content from '../../public/content/header.json';
 
 export const Header = () => {
     return (
@@ -23,7 +12,7 @@ export const Header = () => {
                     <Image src={Logo} width={100} height={50} alt="logo" />
                 </div>
                 <ul className="flex items-center gap-12 font-bold">
-                    {(data as HeaderData).navbarItems.map(item => {
+                    {content.navbarItems.map(item => {
                         return (
                             <li className="group flex items-center gap-2 text-xl hover:text-secondary cursor-pointer duration-100" key={item.text}>
                                 <Icon className="w-6 h-6 group-hover:rotate-[360deg] duration-700" iconName={item.icon as IconProp}/>
