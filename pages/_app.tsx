7347340a6dca78f0c12104b4390f8f1de605b613
Felix <felix.wielander@gmail.com>
2022-10-30 22:12:17 +0100

    Fix hydration errors caused by fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { library } from '@fortawesome/fontawesome-svg-core';
+// using require here because of bug: https://github.com/FortAwesome/Font-Awesome/issues/19348
+const { library, config } = require('@fortawesome/fontawesome-svg-core');
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
+// font awesome icons setup
+config.autoAddCss = false;
 library.add(fas, fab);
 
 function MyApp({ Component, pageProps }: AppProps) {
