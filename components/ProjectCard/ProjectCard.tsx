dc7a544164a7517f23ecb01b7109a2065d2c52c2
Felix <felix.wielander@gmail.com>
2022-10-30 22:00:29 +0100

    Get project cards info from json file

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,31 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
+import { IconReference, ProjectContent } from '../Projects/Projects';
 import Me from '/assets/avatar.svg'
 
 
-export const ProjectCard = () => {
+export const ProjectCard = ({name, description, year, type, references}: ProjectContent) => {
     return (
         <article
             className="relative bg-white overflow-hidden flex flex-col items-center gap-6 min-h-[20rem] w-[18rem] rounded-lg p-4 hover:-translate-y-4 duration-300">
             <div className="absolute top-0 right-0 z-10 p-1">
-                Icons und
+                {references?.map((reference: IconReference) => (
+                    <a key={reference.url} href={reference.url}>
+                        <FontAwesomeIcon icon={reference.icon}/>
+                    </a>
+                ))}
             </div>
             <p className="absolute top-0 left-0 z-10 p-1 font-bold">
-               2021
+               {year}
             </p>
             <div className="absolute top-0 h-[6rem] inset-0 bg-gray-300"></div>
             <div className="w-24 h-24 mt-6">
-                <Image objectFit="cover" className="pt-8" src={Me} alt="Project image"/>
+                <Image className="pt-8 object-contain" src={Me} alt="Project image"/>
             </div>
             <div className="text-center px-8 pb-8 ">
-                <h6>WATTENZÄHLER</h6>
+                <h6>{name?.toUpperCase()}</h6>
                 <p className="text-sm text-gray-500 ">
-                    Point counter for the South Tyrolean card game Watten. (Now replaced by Bleckl)
-                    Point counter for the South Tyrolean card game Watten. (Now replaced by Bleckl)
-                    Point counter for the South Tyrolean card game Watten. (Now replaced by Bleckl)
+                    {description}
                 </p>
             </div>
 
-            <div className="absolute bottom-0 right-0 bg-gray-300 p-1">Flutter Package</div>
+            <div className="absolute bottom-0 right-0 bg-gray-300 p-1">{type}</div>
         </article>
     )
 }
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,18 +1,33 @@
 import {ProjectCard} from '../ProjectCard/ProjectCard';
+import content from '../../public/content/projects.json';
 
+export interface IconReference {
+    icon: string;
+    url: string;
+    tooltip: string;
+}
+
+export interface ProjectContent {
+    name: string;
+    description: string;
+    year: number;
+    type: string;
+    references: IconReference[];
+}
 
 export const Projects = () => {
+    const projects = content as ProjectContent[];
+
+    console.log("read projects", projects);
+    
+
     return (
         <section className="bg-primary py-24">
             <div className="container">
                 <h2 className="text-white mb-24">Projects (Icon for Projects)</h2>
 
                 <div className="flex flex-wrap gap-24 justify-start mx-auto">
-                    <ProjectCard/>
-                    <ProjectCard/>
-                    <ProjectCard/>
-                    <ProjectCard/>
-                    <ProjectCard/>
+                    {projects.map((project: ProjectContent) => (<ProjectCard key={project.name} {...project} />))}
                 </div>
 
 
