d33cb585bd0bf96e28dca0386272a23481bb23c6
Felix <felix.wielander@gmail.com>
2022-11-01 17:54:50 +0100

    Create SmoothScrollLink component and use it in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import content from '../../public/content/header.json';
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import SmoothScrollLink from '../SmoothScrollLink';
 
 interface NavbarItem {
     text?: string;
@@ -23,17 +24,17 @@ export const Header = () => {
     return (
         <header className="fixed w-full h-fit px-2 py-4 bg-white shadow-md z-20">
             <nav className=" container flex justify-between items-center gap-12 text-gray-500">
-                <Link to="/" smooth={true} className="hover:cursor-pointer">
+                <SmoothScrollLink to="/" className="hover:cursor-pointer">
                     <Image src={Logo} width={100} height={50} alt="logo" />
-                </Link>
+                </SmoothScrollLink>
                 <ul className="flex items-center gap-12 font-bold">
                     {headerContent.items.map(item => {
                         return (
                             <li className="group flex items-center gap-2 text-xl hover:text-secondary cursor-pointer duration-100" key={item.text}>
-                                <Link to={item.url ?? ""} className="flex space-x-2" smooth={true}>
+                                <SmoothScrollLink to={item.url ?? ""} className="flex space-x-2">
                                     <FontAwesomeIcon className="w-6 h-6 group-hover:rotate-[360deg] duration-700" icon={item.icon as IconProp}/>
                                     <span>{item.text}</span>
-                                </Link>
+                                </SmoothScrollLink>
                             </li>
                         )
                     })}
