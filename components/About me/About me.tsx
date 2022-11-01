a9f0989df57f55fce1467d2369b9a3010d65b052
Maxwell929 <maximilian9219@gmail.com>
2022-11-01 17:30:54 +0100

    TASK: add prose

--- components/About me/About me.tsx
+++ components/About me/About me.tsx
@@ -1,31 +1,30 @@
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
-import { useContentFromFile } from '../../hooks/useContentFromFile';
+import {useContentFromFile} from '../../hooks/useContentFromFile';
 import Me from '/assets/me.jpeg'
 import {Icon} from '../Icon/Icon';
 
 export const AboutMe = () => {
-    const content = useContentFromFile("content/about.md");
+    const content = useContentFromFile('content/about.md');
 
     return (
         <>
-        <section className="pb-40 xl:pb-56 pt-12 lg:pt-24 bg-secondary">
-            <div className="container">
-                    <div className="flex items-end gap-2 text-white mb-2 lg:mb-12">
-                        <Icon iconName="person" className="h-8 md:h-10 w-8 md:w-10 mb-2"/>
+            <section className="pb-40 md:pb-64 pt-12 lg:pt-24 bg-secondary">
+                <div className="container">
+                    <div className="flex items-end gap-2 text-white mb-4 lg:mb-12">
+                        <Icon iconName="person" className="h-8 md:h-10 w-8 md:w-10 mb-2 hover:scale-110"/>
                         <h2>About me</h2>
                     </div>
-                <div className="flex flex-col lg:flex-row lg:justify-between ">
-                    <div className="lg:w-1/2  text-white mb-8 lg:mb-0">
-                        <ReactMarkdown className="space-y-6">{content}</ReactMarkdown>
-                    </div>
-
+                    <div className="flex flex-col lg:flex-row lg:justify-between ">
+                        <div className="w-full lg:w-1/2 text-3xl text-white lg:mb-0 mb-8 ">
+                            <ReactMarkdown className="prose text-white">{content}</ReactMarkdown>
+                        </div>
                         <div
                             className=" lg:block relative self-center after:hidden lg:after:block lg:after:content-[''] after:ml-6 after:top-4 after:-mr-6 after:-mb-6 after-w-full after:inset-0 after:absolute after:border-4 after:border-white ">
-
                             <Image
-                                className="object-cover z-10 relative hover:scale-110 hover:transition duration-300 delay-100"
-                                src={Me} alt="Profile"/>
+                                className="object-cover z-10 relative md:hover:scale-110 md:hover:transition duration-300 delay-100"
+                                src={Me}
+                                alt="Profile"/>
                         </div>
                     </div>
 
