02e23c1535b2e8ef49e5e1203e974da2176b8200
Maxwell929 <maximilian9219@gmail.com>
2022-11-01 17:32:15 +0100

    TASK: restyle card + other optimizations

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Logo from '/assets/logo.svg'
 import NextLogo from '/assets/next-logo.png'
 import {IconList} from '../IconList/IconList';
+import {Logo} from '../Logo/Logo';
 
 // example data
 
@@ -22,12 +22,12 @@ export const Footer = () => {
     return (
         <section className="py-8">
             <div className="container flex flex-col items-center ">
-                <Image alt="Logo" className="" height={50} width={110} src={Logo}/>
+                <Logo/>
                 <IconList icons={icons} className="gap-8 text-gray-500 py-6"/>
                 <p className="text-sm">{`${new Date().getFullYear()} Made by Maximilian Stelzl and Felix Wielander`}</p>
                 <div className="flex items-center gap-2">
                     <p className="text-sm">Powered by</p>
-                    <Image alt="Next Logo" className="h-8 w-8" width={75} height={50} src={NextLogo}/>
+                    <Image alt="Next Logo" width={75} height={50} src={NextLogo}/>
                 </div>
             </div>
         </section>
--- components/IconList/IconList.tsx
+++ components/IconList/IconList.tsx
@@ -3,13 +3,14 @@ import {Icon, iconProps} from '../Icon/Icon';
 interface iconListProps {
     icons: Array<iconProps>;
     className: string;
+    iconClassName?: string;
 }
 
-export const IconList = ({icons, className}: iconListProps) => {
+export const IconList = ({icons, className, iconClassName}: iconListProps) => {
     return (
         <div className={'flex ' + className}>
             {icons.map(icon =>
-                <Icon iconName={icon.iconName} url={icon.url} className="w-6 md:w-8 h-6 md:h-8" key={icon.url}/>
+                <Icon iconName={icon.iconName} url={icon.url} className={'w-6 md:w-8 h-6 md:h-8 ' + iconClassName} key={icon.url}/>
             )}
         </div>
     )
