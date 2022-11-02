e3a67940847d0c5598dc5dec859b160a09a60f93
Felix <felix.wielander@gmail.com>
2022-11-02 20:09:05 +0100

    Replace !url with undefined checks in Icon.tsx

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -11,9 +11,9 @@ export interface iconProps {
 export const Icon = ({ iconName, className, url }: iconProps) => {
     return (
         <a
-            className={!url ? 'pointer-events-none' : ''}
-            href={!url ? 'javascript:void(0);' : `https://${url}`}
-            target={!url ? '' : '_blank'}
+            className={url === undefined ? 'pointer-events-none' : ''}
+            href={url === undefined ? 'javascript:void(0);' : `https://${url}`}
+            target={url === undefined ? '' : '_blank'}
             rel="noreferrer"
         >
             <FontAwesomeIcon className={className} icon={iconName} />
