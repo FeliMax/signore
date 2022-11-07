f6fc1b439b95cbe8b3b8e5483cc2aa102947e3c3
Felix <felix.wielander@gmail.com>
2022-11-07 21:53:40 +0100

    Remove text-[.7rem] from tooltip classes

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -6,7 +6,7 @@ interface TooltipProps {
 
 export const Tooltip = ({ tooltipName }: TooltipProps) => {
     return (
-        <div className="pointer-events-none absolute  left-1/2 top-[calc(100%+.5rem)] z-20 hidden w-28 -translate-x-1/2 rounded-lg bg-gray-700 p-2 text-center text-[.7rem] text-sm text-white opacity-0 duration-300 group-hover:opacity-100 xl:block">
+        <div className="pointer-events-none absolute left-1/2 top-[calc(100%+.5rem)] z-20 hidden w-28 -translate-x-1/2 rounded-lg bg-gray-700 p-2 text-center text-xs text-white opacity-0 duration-300 group-hover:opacity-100 xl:block">
             {tooltipName}
         </div>
     );
