d64ca2d9351083c71cb23c3ad4fd289387eb6ecd
Felix <felix.wielander@gmail.com>
2022-11-18 21:49:59 +0100

    Footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -4,28 +4,18 @@ import { IconList } from '../IconList';
 import SmoothScrollLink from '../SmoothScrollLink';
 import { Logo } from '../Logo';
 import { IconProps } from '../Icon';
+import content from '../../public/content/footer.json';
 
 // example data
 
-export const icons: IconProps[] = [
-    {
-        iconName: 'fab fa-github',
-        url: 'github.com',
-        tooltip: 'Github',
-    },
-    {
-        iconName: 'fab fa-linkedin',
-        url: 'linkedin.com',
-        tooltip: 'Linkedin',
-    },
-    {
-        iconName: 'fab fa-gitlab',
-        url: 'gitlab.com',
-        tooltip: 'Gitlab',
-    },
-];
+interface FooterContent {
+    iconList: IconProps[];
+    copyrightNotice: string;
+}
 
 export const Footer = () => {
+    const footerContent = content as FooterContent;
+
     return (
         <section className="py-8">
             <div className="container flex flex-col items-center gap-4">
@@ -33,11 +23,13 @@ export const Footer = () => {
                     <Logo />
                 </SmoothScrollLink>
                 <IconList
-                    icons={icons}
-                    className="gap-8 py-6 text-gray-500"
-                    iconClassName="w-10 h-10 hover:scale-110 hover:text-gray-300 duration-300"
+                    icons={footerContent.iconList}
+                    className="gap-8 py-6 text-primary"
+                    iconClassName="w-10 h-10 hover:scale-105 duration-300 hover:text-secondary"
                 />
-                <p className="text-center text-sm">{`${new Date().getFullYear()} Made by Maximilian Stelzl and Felix Wielander`}</p>
+                <p className="text-center text-sm">
+                    {footerContent.copyrightNotice}
+                </p>
                 <div className="flex items-center gap-2">
                     <p className="text-sm">Powered by</p>
                     <ExportedImage
