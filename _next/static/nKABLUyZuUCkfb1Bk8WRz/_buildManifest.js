0c0d9c91bf4207c4c036cad881640ee159b35587
Maxwell929 <Maxwell929@users.noreply.github.com>
2022-11-19 18:29:14 +0000

    deploy: 700ee511bbd6580cdef8ecd2e06cef5acb7199ee

---
+++ _next/static/nKABLUyZuUCkfb1Bk8WRz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/265-b2fe99d54c59cd4f.js","static/chunks/pages/index-eafd7d2f41d0b386.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
